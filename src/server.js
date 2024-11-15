b879ebf003104eab0e5df09ccd49faf9a071274b
Yuliia Voziian <vozianulia@gmail.com>
2024-11-15 17:49:04 +0200

    update

--- src/server.js
+++ src/server.js
@@ -4,12 +4,14 @@ import cors from 'cors';
 
 export const setupServer = () => {
   const app = express();
+  const PORT = process.env.PORT || 3000;
 
+  // Middleware для роботи з JSON
   app.use(express.json());
-  //Налаштування cors
+  // Налаштування CORS
   app.use(cors());
 
-  //Налаштування логгера pino.
+  // Налаштування логера
   app.use(
     pino({
       transport: {
@@ -24,20 +26,12 @@ export const setupServer = () => {
     });
   });
 
-  app.use((req, res, next) => {
+  app.use('*', (req, res, next) => {
     res.status(404).json({
       message: 'Not found',
     });
   });
 
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
-
-  const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
