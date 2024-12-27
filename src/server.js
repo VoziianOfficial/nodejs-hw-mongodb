239f1cc838f7b57b6e1294da111089483bca8902
Yuliia Voziian <vozianulia@gmail.com>
2024-12-28 01:41:30 +0200

    fix:

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 export const setupServer = () => {
   const app = express();
@@ -39,6 +40,12 @@ export const setupServer = () => {
   });
 
   app.use('/uploads', express.static(UPLOAD_DIR));
+
+  app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 };
+
+
+
 
 export default setupServer;
